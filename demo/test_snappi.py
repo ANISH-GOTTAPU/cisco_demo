3996ccb741dde84ec55342280e61b5bc0176a571
automation <anish.gottapu@keysight.com>
2023-03-21 15:32:25 +0530

    update test_snappi

--- demo/test_snappi.py
+++ demo/test_snappi.py
@@ -182,7 +182,7 @@ def test_snappi(request):
     ts.state = ts.START
     api.set_transmit_state(ts)
 
-    time.sleep(3)
+    time.sleep(5)
     wait_for(
         fn=lambda: flow_metrics_ok(api), fn_name="wait_for_flow_metrics"
     )
